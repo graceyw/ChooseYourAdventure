a73bcd357c4be492660eb30d4fb0b05789a7c2e2
Gracey Wilson <gracey.wilson@students.olin.edu>
2020-03-01 17:14:51 -0500

    MVP file structure set up

---
+++ madlibMVP.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  Input:
+  Output:
+*/
+char find_madlib(num) {
+  // TODO MJ
+  copyMad = copy(emptyMad) //copy doesn't actually work, must write it
+  return copyMad;
+}
+
+/*
+  Input:
+  Output:
+*/
+char parse_madlib(copyMad) {
+  // TODO Gracey
+  return completedMad;
+}
+
+/*
+  Input:
+  Output:
+*/
+int main() {
+  // TODO together
+  fgets("Type in a number: one or two ")
+  //save the input from fgets as num
+  emptyMad = find_madlib(num);
+  completedMad = parse_madlib(emptyMad);
+  printf("%s\n", completedMad);
+  return 0;
+}
