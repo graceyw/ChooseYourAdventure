d3375a2b346ae48c2d937a2c8eb814fe5326bb81
MJ-McMillen <16mmcmillen.stem@gmail.com>
2020-04-09 18:00:21 -0400

    MUAHAHAHA

--- MakeMadlibsGame.c
+++ MakeMadlibsGame.c
@@ -7,30 +7,30 @@
 #define NUMBER_OF_MADLIBS  3
 
 // first Madlib
-char Mad1[60] = "The 1 NINJA said 2 to the 3 students to 4 their homework.";
-char key1[4][9] = {"adjective", "adverb", "adjective", "verb"};
-//char *key1p = key1;
+char Mad1[60] = "The 1 NINJA said 2 to the 3 student to go 4 their homework.";
+char key1[4][15] = {"adjective", "adverb", "adjective", "verb"};
+
 
 // second Madlib
 char Mad2[110] = "Allen Downey, Professor of 1 at Olin College of 2, is teaching a 3 class called 4 Systems this Fall.";
-char key2[4][9] = {"noun","noun","adjective","noun"};
-//char *key2p = key2;
+char key2[4][15] = {"noun","noun","adjective","noun"};
 
 // third Madlib
 char Mad3[110] = "Head First 1 is a very 2 book about 3 oriented programming.The 2 book is as 4 as is it informative.";
-char key3[4][9] = {"noun","adjective", "noun","adverb"};
-//char *key3p = key3;
+char key3[4][15] = {"letter","adjective", "noun","adverb"};
+
 
 //Madlib Library of pointers.
-char *Mad_List[NUMBER_OF_MADLIBS-1];
-char *Mad_Keys[NUMBER_OF_MADLIBS-1];
+char *Mad_List[NUMBER_OF_MADLIBS];
+char master_key[3][4][15] = {{"adjective", "adverb", "adjective", "verb"},{"noun","noun","adjective","noun"},{"letter","adjective", "noun","adjective"}};
+//char *Mad_Keys[NUMBER_OF_MADLIBS];
 
 void print_madlib(int num, char **answers)
 {
   int i;
   char m;
   printf("Your MadLib is... drumroll, please... \n\n");
-  for(i = 0; i < strlen(Mad_List[num-1]);i++) // For the length of our madlib string...
+  for(i = 0; i < strlen(Mad_List[num-1])+1;i++) // For the length of our madlib string...
   {
     m = Mad_List[num-1][i];    // Grab the current character being processed
     if(atoi(&m)==0)       // If that character is not an integer...
@@ -50,34 +50,38 @@ int main()
     Mad_List[0] = Mad1;
     Mad_List[1] = Mad2;
     Mad_List[2] = Mad3;
-    Mad_Keys[0] = *key1;
-    Mad_Keys[1] = *key2;
-    Mad_Keys[2] = *key3;
-    // char *mad_keys[NUMBER_OF_MADLIBS-1] = {key1p,key2p,key3p};
+
+
     char m = Mad_List[0][0];
-    char k = Mad_Keys[0][0];
-    // printf("%c\n",k);
 
     // HERE BEGINS the input/fgets stuff
     char input[15];
-    char answers[4][10];
+    char answers[4][15];
     int index;
     char madNum;
+    int intmadNum;
 
     // User selects a madlib by number
-    printf("Please enter an integer between 0 and 2 to select a madlib to play: ");
+    printf("Please enter an integer between 1 and %d to select a madlib to play: ",NUMBER_OF_MADLIBS);
     fgets(&madNum, 15, stdin);
-    printf("%c\n", madNum);  //print statement for testing purposes
+
 
     // We collect and store user input for selected madlib
+    intmadNum = madNum-'0';
+    printf("%d\n", intmadNum);
     for (int i=0; i<4; i++) {
-      printf("Please enter a(n) %s: ", key2[i]); // Prompt user input - needs to change to Mad_Keys[madNum][i] or something like that. and we need to decide on either 0 or 1 indexing.
+      printf("Please enter a(n) "); //prompt user input
+      for(int r = 0;r<15;r++){ //this for loop prints out the letters stored in our 3d array.
+       printf("%c",master_key[intmadNum-1][i][r]);
+      }
+      printf("\n");
       fgets(input, 15, stdin);                   // Collect user input via stdin and store in "input" array
-      printf("%s", input);                    // print statement for testing purposes
+    //  printf("%s", input);                    // print statement for testing purposes
       for (int j=0; j<15; j++) {                 // For each character in the input word...
         answers[index][j] = input[j];            // Add the inputted char to the answers array
         // printf("%c", answers[index][j]);   // print statement for testing purposes
       }
+
       index++;
     }
 
@@ -87,6 +91,6 @@ int main()
     rel_answers[1] = answers[1];
     rel_answers[2] = answers[2];
     rel_answers[3] = answers[3];
-    // print_madlib(1,rel_answers); // por que no solamente answers??
+    print_madlib(intmadNum,rel_answers); // por que no solamente answers??
     return 0;
   }
